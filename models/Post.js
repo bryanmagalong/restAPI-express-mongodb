894014faf6b13cdcf5ad0bc9a72295d456990a8c
Bryan M <allen.bry95@gmail.com>
2020-05-20 16:28:43 +0200

    :construction: post request route added, Post model added

---
+++ models/Post.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+//== Posts Model
+const PostSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// 1rt param: Model name
+// 2nd param: Schema to use
+module.exports = mongoose.model('Post', PostSchema);
--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,17 @@
 const express = require('express');
-
 const router = express.Router();
+const Post= require('../models/Post');
+
 // We can omit '/posts' since we only get here
 // when we are on '/posts' route
 
 // route: '/posts'
 router.get('/', (req, res) => {
   res.send('We are on posts !');
+});
+
+router.post('/', (req, res) => {
+  console.log(req.body);
 });
 
 // route: '/posts/specific'
