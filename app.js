0d2e5530fecaa6d1b908edcc5f8b74b7583034b0
Bryan M <allen.bry95@gmail.com>
2020-05-20 16:08:29 +0200

    :construction: posts routes and middleware

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+
+// To access env variables
 require('dotenv/config');
 
+//== Import routes
+const postsRoute = require('./routes/posts');
+
 //== Middlewares
-app.use('/posts', () => {
-    console.log('//== Posts middleware running...')
-});
+// Everytime we go to /posts routes, we use postRoutes
+// We can now use multiple routes with '/posts' base url
+// for example: '/posts/specific' -> all you need is to create a route '/specific' in './routes/posts'
+app.use('/posts', postsRoute);
 
 //== Routes
 app.get('/', (req, res) => {
-    res.send('Hello World !');
-});
-
-app.get('/posts', (req, res) => {
-    res.send('We are on posts !');
+  res.send('Hello World !');
 });
 
 // Connect to DB
-mongoose.connect(process.env.DB_CONNECTION,
-    { 
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-    },
-    () => {
-    console.log('Connected to DB');
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { 
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  },
+  () => {
+  console.log('Connected to DB');
 });
 
 // We start listening port 3000
